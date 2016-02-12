add4a29c446f73bd1cc08bbe537b4ae452acd60d
Daichi Kimura <dkimura@bdd.xyz>
2016-02-13 00:25:22 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# heroku-pipeline-sandbox
+
+## install
+
+```
+$ npm install
+```
+
+## usage
+
+```
+$ npm start
+```
+
+open [http://localhost:3000](http://localhost:3000)
---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(`<h1>Hello ${ process.env.NODE_ENV }</h1>`);
+});
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Start Server');
+});
